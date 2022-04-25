87e8efb508af90a59b3036e306e0b10279ce4f6c
Denes <jose.araujo@zup.com.br>
2022-04-25 16:34:59 -0300

    commit inicial

---
+++ src/main/java/br/com/zup/edu/zupstore/ZupstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.zupstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupstoreApplication.class, args);
+	}
+
+}
